284bee5dd548479162676bbf843bec7750a2e4b7
SrGexj <juano@bittacora.com>
2025-02-28 10:39:21 +0100

    updated controller
    Updated controller to bring absolute URL to the json response. Changed the way to add the params.

--- controllers/upload_controller.js
+++ controllers/upload_controller.js
@@ -4,31 +4,45 @@ const fs = require('fs')
 
 const compressImage = async (req, res) => {
     try {
-        const images = req.files // Ahora `req.files` es un array
-        const { resize, format, quality, lossless} = req.body
+        const images = req.files 
+        let { resize, format, quality, lossless} = req.body
 
         if (!images || images.length === 0) {
             return res.status(400).json({ message: 'No images uploaded' })
         }
 
+        resize = parseInt(resize) || 1500
+        format = format || 'webp'
+        quality = parseInt(quality) || 80
+        lossless = lossless === 'true'
+
         const compressedImages = []
+
+        const uploadDir = path.join(__dirname, '../uploads');
+        if (!fs.existsSync(uploadDir)) {
+          fs.mkdirSync(uploadDir);
+        }
 
         const name = path.parse(images[0].filename).name
         const sinResolucion = name.replace(/[\s-]*\d+x\d+\s*$/, '');
         const nombreFinal = sinResolucion.replace(/\s+/g, '-');
 
-        // Procesar todas las imágenes
         for (const image of images) {
-            const compressedImagePath = `./uploads/compressed-${nombreFinal}-${resize ? resize : '1500'}.${format}`
+            // const compressedImagePath = `./uploads/compressed-${nombreFinal}-${resize ? resize : '1500'}.${format}`
+            const compressedFileName = `compressed-${nombreFinal}-${resize}.${format}`;
+            const compressedFilePath = path.join(uploadDir, compressedFileName);
+
 
             await sharp(image.path)
-                .resize({ width: parseInt(resize) ? parseInt(resize) : 1500 })
-                .toFormat(format ? format : 'webp')
+                .resize({ width: resize ? resize : 1500 })
+                .toFormat( format ? format : 'webp')
                 .webp({ quality: parseInt(quality ? quality : 80 ), lossless: lossless === 'true' ? true : false })
-                .toFile(compressedImagePath)
+                .toFile(compressedFilePath)
 
-            compressedImages.push(compressedImagePath)
+                const publicUrl = `${req.protocol}://${req.get('host')}/uploads/${compressedFileName}`;
+                compressedImages.push(publicUrl);                
         }
+
 
         res.status(200).json({ message: 'Images compressed', files: compressedImages })
     } catch (error) {
